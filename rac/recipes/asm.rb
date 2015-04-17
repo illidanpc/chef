f68817c9fcbd1127d1279e9afc0c62cf6fc78867
illidanpc <freedomzm@126.com>
2015-04-17 13:46:19 +0800

    2.2.7
    update asm

--- rac/recipes/asm.rb
+++ rac/recipes/asm.rb
@@ -2,17 +2,17 @@ unless node['rac']['grid']['asm']['flag']
 
 execute "asm_data" do
   cwd "#{node['rac']['grid']['home']}/bin"
-  command "sudo -Eu grid ./asmca -silent -createDiskGroup -diskString '#{node['rac']['grid']['asm']['disk_string']}' -diskGroupName DATA_DG -diskList /dev/asmdisk/db_data_01 -redundancy EXTERNAL -au_size #{node['rac']['grid']['asm']['au_size']} -sysAsmPassword oracle"
+  command "sudo -Eu grid ./asmca -silent -createDiskGroup -diskString '#{node['rac']['grid']['asm']['disk_string']}' -diskGroupName DATA_DG -diskList /dev/asmdisk/#{node['rac']['oracle']['dbname']}_data_01 -redundancy EXTERNAL -au_size #{node['rac']['grid']['asm']['au_size']} -sysAsmPassword oracle"
 end
 
 execute "asm_arch" do
   cwd "#{node['rac']['grid']['home']}/bin"
-  command "sudo -Eu grid ./asmca -silent -createDiskGroup -diskString '#{node['rac']['grid']['asm']['disk_string']}' -diskGroupName ARCH_DG -diskList /dev/asmdisk/db_arch_01 -redundancy EXTERNAL -au_size #{node['rac']['grid']['asm']['au_size']} -sysAsmPassword oracle"
+  command "sudo -Eu grid ./asmca -silent -createDiskGroup -diskString '#{node['rac']['grid']['asm']['disk_string']}' -diskGroupName ARCH_DG -diskList /dev/asmdisk/#{node['rac']['oracle']['dbname']}_arch_01 -redundancy EXTERNAL -au_size #{node['rac']['grid']['asm']['au_size']} -sysAsmPassword oracle"
 end
 
 execute "asm_redo" do
   cwd "#{node['rac']['grid']['home']}/bin"
-  command "sudo -Eu grid ./asmca -silent -createDiskGroup -diskString '#{node['rac']['grid']['asm']['disk_string']}' -diskGroupName REDO_DG -diskList /dev/asmdisk/db_redo_01 -redundancy EXTERNAL -au_size #{node['rac']['grid']['asm']['au_size']} -sysAsmPassword oracle"
+  command "sudo -Eu grid ./asmca -silent -createDiskGroup -diskString '#{node['rac']['grid']['asm']['disk_string']}' -diskGroupName REDO_DG -diskList /dev/asmdisk/#{node['rac']['oracle']['dbname']}_redo_01 -redundancy EXTERNAL -au_size #{node['rac']['grid']['asm']['au_size']} -sysAsmPassword oracle"
 end
 
 ruby_block 'set_asm_flag' do
